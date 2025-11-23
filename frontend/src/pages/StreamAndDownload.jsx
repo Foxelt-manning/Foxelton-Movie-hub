bd3b474f9f798244247c2103a318a94ad67922f9
Foxelt Manning <foxeltmanning@gmail.com>
2025-11-23 08:07:33 +0000

    Seperated the modules for easy deploy

---
+++ frontend/src/pages/StreamAndDownload.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+import Episodes from '../components/Episodes'
+import { useLocation, useParams } from 'react-router-dom'
+import axios from 'axios'
+import Downloads from '../components/DownloadFiles'
+
+const StreamAndDownload = () => {
+  const [downloadData,setData] = useState(null);
+  const [loading,setLoading]=useState(true);
+  const {subjectId} =useParams()
+
+  const location = useLocation();
+  const queryParams =new URLSearchParams(location.search);
+  const season = queryParams.get("season");
+  const episode = queryParams.get("episode");
+  const maxEp = queryParams.get("maxEp");
+
+
+  useEffect(()=>{
+    const fetchDownloadData = async()=>{
+      setLoading(true);
+      setData(null);
+      try {
+        
+        const res = await axios.get(`http://localhost:3000/api/stream-download/${subjectId}?season=${season}&episode=${episode}`);
+        
+        const downloadData = res.data.downloads;
+        setData(downloadData);
+      } catch (error) {
+        console.error(error)
+      }
+      setLoading(false)
+
+    }
+    fetchDownloadData()
+  },[episode,season,subjectId])
+  console.log(downloadData)
+  
+  return (
+    <>
+    {!loading && downloadData ?(
+      <>
+      <div >
+        <Downloads data={downloadData}/>
+        <Episodes data={downloadData} subjectId={subjectId} season={season} maxEp={maxEp}/>
+      </div>
+      </>
+    ):(<div>
+      Loading ...
+    </div>)}
+   </>
+  )
+}
+
+export default StreamAndDownload
